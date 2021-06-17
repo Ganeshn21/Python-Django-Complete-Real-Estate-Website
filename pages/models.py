09cbfb406ba103c58f9efa2607d3f93b4af573cf
ganesh <ganeshchowdary052@gmail.com>
2021-06-17 20:59:42 +0530

    mergeconflict

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class studnt(models.Model):
+    sno=models.IntegerField()
+    sname=models.CharField(max_length=24)
+
+
+class master(models.Model):
+    sname = models.CharField(max_length=24)
+Brach is upda
+B
+B
+
+C
+C
+C
+    sno = models.IntegerField()
